aa3ff4c9f70ec2fc5600e96ababd5d9293cc47d0
DharshanSR <dharshanravindran8@gmail.com>
2024-07-20 12:02:33 +0530

    all other function like add, delete, share and more

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'img.clerk.com' }]
+  }
+};
