c56f41022140e6bea4bad4bc01d48cee88f05daf
DharshanSR <dharshanravindran8@gmail.com>
2024-07-20 11:52:14 +0530

    main layout page and provider page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import { Inter as FontSans } from "next/font/google"
-import "./globals.css"
-import { Metadata } from "next"
 
 import { cn } from "@/lib/utils"
+import './globals.css'
+import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -14,19 +17,29 @@ export const metadata: Metadata = {
   description: 'Your go-to collaborative editor',
 }
 
-
-export default function RootLayout({ children }: { children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { 
+          colorPrimary: "#3371FF" ,
+          fontSize: '16px'
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
