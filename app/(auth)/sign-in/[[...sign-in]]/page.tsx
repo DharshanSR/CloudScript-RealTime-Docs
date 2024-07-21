8f40f4c2f291fa888803e1281ab87f4f85adb4ee
DharshanSR <dharshanravindran8@gmail.com>
2024-07-21 16:27:59 +0530

    add the comments for easy understand

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,16 @@
+// Import the SignIn component from the @clerk/nextjs package
 import { SignIn } from '@clerk/nextjs'
 
+// Define a functional component called SignInPage
 const SignInPage = () => {
   return (
+    // Return a main element with a class of "auth-page"
     <main className="auth-page">
+      {/* Render the SignIn component from @clerk/nextjs */}
       <SignIn />
     </main>
   )
 }
 
+// Export the SignInPage component as the default export of this module
 export default SignInPage
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,16 @@
+// Import the SignUp component from the @clerk/nextjs package
 import { SignUp } from '@clerk/nextjs'
 
+// Define a functional component called SignUpPage
 const SignUpPage = () => {
   return (
+    // Return a main element with a class of "auth-page"
     <main className="auth-page">
+      {/* Render the SignUp component from @clerk/nextjs */}
       <SignUp />
     </main>
   )
 }
 
+// Export the SignUpPage component as the default export of this module
 export default SignUpPage
